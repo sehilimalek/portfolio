7243cdba1bdb83dfcf6ed5043aadd0ee5cc2f2ff
sehilimalek <sehilimalek@icloud.com>
2023-08-29 00:13:15 +0100

    nav update 2

--- src/_components/navbar.tsx
+++ src/_components/navbar.tsx
@@ -2,7 +2,7 @@ import { ModeToggle } from "./displayMode-toggle";
 
 const Navbar = () => {
   return (
-    <nav className="p-4">
+    <nav className="p-4 flex justify-end">
       <ModeToggle />
     </nav>
   );
